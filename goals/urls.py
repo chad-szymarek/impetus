cbcd8a08c6a3ea1abfd7b7051aaf8c6b027ad315
chad.szymarek <chadszymarek@gmail.com>
2022-04-07 14:00:47 -0600

    add goal to character you selected new goal from

--- goals/urls.py
+++ goals/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from goals.views import CharacterGoalsCreateView, CharacterGoalsUpdateView,CharacterGoalsCompletedUpdateView
 urlpatterns = [
-    path("create/", CharacterGoalsCreateView.as_view(), name="create_character_goal"),
+    path("<int:goal_id>/create/", CharacterGoalsCreateView.as_view(), name="create_character_goal"),
     path("<int:pk>/update/", CharacterGoalsUpdateView.as_view(), name="update_character_goal"),
     path("<int:pk>/complete/", CharacterGoalsCompletedUpdateView.as_view(), name="complete_goal"),
 
